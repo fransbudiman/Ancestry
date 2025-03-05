71c37ea5136c9790791ef0c2d276c5d784060354
Frans Budiman <105959450+fransbudiman@users.noreply.github.com>
2025-03-05 15:44:07 -0500

    Update ancestry_estimation_grafpop.sh
    improve checking for existing grafpop

--- ancestry_estimation_grafpop.sh
+++ ancestry_estimation_grafpop.sh
@@ -1,49 +1,62 @@
 #!/bin/bash
 
-#enable debugging
+# Enable debugging
 set -x
 
-# set arguments
+# Set arguments
 VCF_FILE=$1
 RESULT_FILE=$2
 PNG_FILE=$3
 
 {
-# check if arguments are empty
-if [ -z "$VCF_FILE" ] || [ -z "$RESULT_FILE" ] || [ -z "$PNG_FILE" ]; then
-    echo "Missing arguments. Please provide VCF file, result file, and PNG file."
-    exit 1
-fi
+    # Check if arguments are empty
+    if [ -z "$VCF_FILE" ] || [ -z "$RESULT_FILE" ] || [ -z "$PNG_FILE" ]; then
+        echo "Missing arguments. Please provide VCF file, result file, and PNG file."
+        exit 1
+    fi
 
-# check if grafpop and PlotGrafPopResults.pl executable exist
-GRAFPOP_DIR=$(which grafpop)
-PLOTGRAFPOP_DIR=$(which PlotGrafPopResults.pl)
-if [ -z "$GRAFPOP_DIR" ]; then
-    echo "grafpop executable or PlotGrafPopResults.pl not found. Installing them . . ."
-    wget https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/GetZip.cgi?zip_name=GrafPop1.0.tar.gz
-    tar -xvzf GetZip.cgi?zip_name=GrafPop1.0.tar.gz
-    mkdir GrafPop
-    mv * GrafPop
-    mv GrafPop /opt/
-    sudo ln -s /opt/GrafPop/grafpop /usr/local/bin/grafpop
-    sudo ln -s /opt/GrafPop/PlotGrafPopResults.pl /usr/local/bin/PlotGrafPopResults.pl
-fi
+    # Check if grafpop and PlotGrafPopResults.pl executable exist
+    if [ ! -f "/usr/local/bin/grafpop" ]; then
+        echo "grafpop executable not found. Installing it . . ."
+        
+        # Download and extract GrafPop
+        if ! command -v wget &> /dev/null; then
+            echo "wget is not installed. Please install it."
+            exit 1
+        fi
+        
+        wget https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/GetZip.cgi?zip_name=GrafPop1.0.tar.gz
+        if [ $? -ne 0 ]; then
+            echo "Error downloading GrafPop."
+            exit 1
+        fi
 
-# check perl path and update PlotGrafPopResults.pl shebang
-PERL_PATH=$(which perl)
-if [ -z "$PERL_PATH" ]; then
-    echo "perl not found. Please install perl."
-    exit 1
-else
-    sed -i "1s|^#!.*|#!$PERL_PATH|" /opt/GrafPop/PlotGrafPopResults.pl
-    sudo cpan GD::Text
-    sudo cpan GD::Graph
-fi
+        tar -xvzf GetZip.cgi?zip_name=GrafPop1.0.tar.gz
+        if [ $? -ne 0 ]; then
+            echo "Error extracting the tarball."
+            exit 1
+        fi
+        
+        mkdir GrafPop
+        mv * GrafPop
+        mv GrafPop /opt/
+        sudo ln -s /opt/GrafPop/grafpop /usr/local/bin/grafpop
+        sudo ln -s /opt/GrafPop/PlotGrafPopResults.pl /usr/local/bin/PlotGrafPopResults.pl
+    fi
 
-# to run: grafpop <vcf_file> <result_file>
-grafpop $1 $2
+    # Check perl path and update PlotGrafPopResults.pl shebang
+    PERL_PATH=$(which perl)
+    if [ -z "$PERL_PATH" ]; then
+        echo "perl not found. Please install perl."
+        exit 1
+    else
+        sed -i "1s|^#!.*|#!$PERL_PATH|" /opt/GrafPop/PlotGrafPopResults.pl
+        sudo cpan GD::Text
+        sudo cpan GD::Graph
+    fi
 
-# to plot: PlotGrafPopResults.pl <result_file> <png_file>
-PlotGrafPopResults.pl $2 $3
+    # Run grafpop and PlotGrafPopResults.pl
+    grafpop "$VCF_FILE" "$RESULT_FILE"
+    PlotGrafPopResults.pl "$RESULT_FILE" "$PNG_FILE"
 
 } 2>&1 | tee -a grafpop.log
