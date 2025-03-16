a5a91454e75c81341744c8d2044e31c2e0680c93
Frans Budiman <105959450+fransbudiman@users.noreply.github.com>
2025-03-16 18:16:44 -0400

    Update admixture_pie.R
    update method of finding our patient sample

--- admixture_pie.R
+++ admixture_pie.R
@@ -107,7 +107,7 @@ for (region in regions){
 ## graph patient sample
 # patient <- results[is.na(results$Region),]
 # New method to define patient
-patient <- results[results$name == sample_name & is.na(results$Region), ]
+patient <- results[grepl("(_HapMap3|_1KGenomes)$", results$name) & is.na(results$Region), ]
 dat <- melt(patient, id=c("Region", "name"))
 dat$value <- signif(dat$value * 100, digits=3)
 dat$Labels <- sapply(dat$value, function(x){
