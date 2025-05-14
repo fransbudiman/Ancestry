54c8c7327902c2f86d39c3bc023886db8e47ff2b
root <root@LAPTOP-F3HJ306J>
2025-05-13 22:12:00 -0400

    script to find samples with missing result, then create a directory of soft link of the missing samples to be resubmitted

---
+++ JobScripts/find_missing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Enable debugging
+set -x
+
+# Set arguments
+# o = original directory
+# r = result directory
+while getopts ":o:r:" flag
+do
+    case "${flag}" in
+        o) ORIGINAL_DIR=${OPTARG};;
+        r) RESULT_DIR=${OPTARG};;
+        \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+        :)  echo "Missing argument for -$OPTARG" >&2; exit 1 ;;
+    esac
+done
+
+# check if directory exists
+if [ ! -d "$ORIGINAL_DIR" ]; then
+    echo "Original directory does not exist."
+    exit 1
+fi
+if [ ! -d "$RESULT_DIR" ]; then
+    echo "Result directory does not exist."
+    exit 1
+fi
+
+ls $ORIGINAL_DIR/* -type d | awk -F/ '{print $(NF-1)}' > $RESULT_DIR/all_samples_temp.txt
+ls $RESULT_DIR/*_ancestry.txt -type f | awk -F/ '{print $(NF-1)}' | sed 's/_ancestry.txt//' > $RESULT_DIR/completed_samples_temp.txt
+
+# Compare the two files and find missing samples
+comm -23 <(sort $RESULT_DIR/all_samples_temp.txt) <(sort $RESULT_DIR/completed_samples_temp.txt) > $RESULT_DIR/missing_samples.txt
+rm $RESULT_DIR/*_temp.txt
+
+cat $RESULT_DIR/missing_samples.txt
+
+
