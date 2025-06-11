dce6777c10694d3a5959cd12ea05f85dbe74ee7f
Frans Budiman <fransrewl@gmail.com>
2025-06-11 11:16:53 -0400

    add parsers

--- Plot/bubbleplot.py
+++ Plot/bubbleplot.py
@@ -8,23 +8,39 @@ import pandas as pd
 import plotly.express as px
 # for counting pairs
 from collections import Counter
+# for command line argument parsing
+import argparse
+
+# default values
+col1_label = "HapMap3_Erika"
+col2_label = "Self-reported_Ancestry"
+file_path = "C:\\UNIFRANS\\Work\\JLE\\temp\\GENCOV_Result1.csv"
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', default=file_path, help='Path to the CSV file containing the data')
+parser.add_argument('-c1', '--col1', default=col1_label, help='Column name for the first axis')
+parser.add_argument('-c2', '--col2', default=col2_label, help='Column name for the second axis')
+args = parser.parse_args()
+
+col1_label = args.col1
+col2_label = args.col2
+file_path = args.file
 
 # function to read data from csv file
 def read_csv(file_path, col1, col2):
     pairlist = []
     dataframe = pd.read_csv(file_path, engine='python')
-    col1 = "HapMap3_Erika"
-    col2 = "Self-reported_Ancestry"
     target = dataframe[[col1, col2]]
     for index, row in target.iterrows():
         result_tuple = (row[col1], row[col2])
         pairlist.append(result_tuple)
     return pairlist
 
-sampledata = read_csv("C:\\UNIFRANS\\Work\\JLE\\temp\\GENCOV_Result1.csv", "HapMap3_Erika", "Self-reported_Ancestry")
+sampledata = read_csv(file_path, col1_label, col2_label)
 
-xaxis = "HapMap3_Erika"
-yaxis = "Self-reported_Ancestry"
+xaxis = col1_label
+yaxis = col2_label
 
 counter = Counter(sampledata)
 print(counter) # for debugging remove later
@@ -49,5 +65,5 @@ fig = px.scatter(
 )
 
 fig.show()
-fig.write_html("plot.html")
-fig.write_image("plot.png") # uncomment to save as png
+fig.write_html(f"bubble_{col1_label}_{col2_label}.html")
+# fig.write_image(f"bubble_{col1_label}_{col2_label}.png") # uncomment to save as png
