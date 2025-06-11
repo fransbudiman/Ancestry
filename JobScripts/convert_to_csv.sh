4c022cdb44e5b8909446db474c35fbbd21bd838a
Frans Budiman <fransrewl@gmail.com>
2025-06-11 15:28:24 -0400

    tryig to debug

--- JobScripts/convert_to_csv.sh
+++ JobScripts/convert_to_csv.sh
@@ -49,60 +49,66 @@ counter=0
 
 if [ "$REF" = "Hapmap" ] || [ "$REF" = "1KGenomes" ]; then
   # Search for all files that contain a *top3.txt file
-for file in $(find $TARGET_DIR -type f -name "*top3.txt")
-  do
-      counter=$((counter+1))
-      echo "Processing file: $file"
-      # get the name of the file
-      FILE_NAME=$(basename $file)
-      # get the sample name
-      SAMPLE=$(echo $FILE_NAME | cut -d'_' -f1)
-      # get the ancestry values
-      ANCESTRY=$(awk 'NR==1 {for (i=1; i<=NF; i++) header[i]=$i} NR==2 {for (i=1; i<=NF; i++) printf "%s=%.6f%s", header[i], $i, (i<NF ? " " : "")}' $file)
-      # echo "debugging: $ANCESTRY"
-      # get top 1 ancestry values
-      top_ancestry=$(echo "${ANCESTRY}" | cut -d'=' -f1)
-      # Search if the sample is already in the csv file
-      if ! grep -q "$SAMPLE" "$CSV"; then
-          echo "cannot find sample in csv file, adding it"
-          # Add the sample to the csv file
-          # differentiate is sample is named after TCAGID or StudyID
-          if [[ "$SAMPLE" == *-* ]]; then
-              echo "Sample is a TCAGID: $SAMPLE"
-              echo ",$SAMPLE,,,,,,,,,,,,,,,," >> $CSV
-              echo "$SAMPLE" >> "missing_samples.txt"
-          else
-              echo "Sample is a StudyID: $SAMPLE"
-              echo "$SAMPLE,,,,,,,,,,,,,,,,," >> $CSV
-              echo "$SAMPLE" >> "missing_samples.txt"
-          fi
-          
-      fi
+  for file in $(find $TARGET_DIR -type f -name "*top3.txt")
+    do
+        counter=$((counter+1))
+        echo "Processing file: $file"
+        # get the name of the file
+        FILE_NAME=$(basename $file)
+        # get the sample name
+        SAMPLE=$(echo $FILE_NAME | cut -d'_' -f1)
+        # get the ancestry values
+        ANCESTRY=$(awk 'NR==1 {for (i=1; i<=NF; i++) header[i]=$i} NR==2 {for (i=1; i<=NF; i++) printf "%s=%.6f%s", header[i], $i, (i<NF ? " " : "")}' $file)
+        # echo "debugging: $ANCESTRY"
+        # get top 1 ancestry values
+        top_ancestry=$(echo "${ANCESTRY}" | cut -d'=' -f1)
+        # Search if the sample is already in the csv file
+        if ! grep -q "$SAMPLE" "$CSV"; then
+            echo "cannot find sample in csv file, adding it"
+            # Add the sample to the csv file
+            # differentiate is sample is named after TCAGID or StudyID
+            if [[ "$SAMPLE" == *-* ]]; then
+                SAMPLE=$(sanitize "$SAMPLE")
+                echo "Sample is a TCAGID: $SAMPLE"
+                echo ",$SAMPLE,,,,,,,,,,,,,,,," >> $CSV
+                echo "$SAMPLE" >> "missing_samples.txt"
+            else
+                SAMPLE=$(sanitize "$SAMPLE")
+                echo "Sample is a StudyID: $SAMPLE"
+                echo "$SAMPLE,,,,,,,,,,,,,,,,," >> $CSV
+                echo "$SAMPLE" >> "missing_samples.txt"
+            fi
+            
+        fi
 
-      # Store sample ancestry value in an array
-      row=$(grep -n "$SAMPLE" "$CSV")
-      line_number=$(echo $row | cut -d':' -f1)
-      sample_data=$(echo $row | cut -d':' -f2-)
-      IFS=',' read -r -a row_array <<< "$sample_data"
-      # echo "debugging: ${row_array[*]}"
-      
-      # Add the ancestry values to the csv file based on the reference dataset
-      if [[ $REF == "Hapmap" ]]; then
-          row_array[13]=${top_ancestry}
-          row_array[14]=${ANCESTRY}
-      elif [[ $REF == "1KGenomes" ]]; then
-          row_array[15]=${top_ancestry}
-          row_array[16]=${ANCESTRY}
-      elif [[ $REF == "GrafPop" ]]; then
-          # to implement ...
-          echo "not implemented"
-      fi
+        # Store sample ancestry value in an array
+        row=$(grep -n "$SAMPLE" "$CSV")
+        line_number=$(echo $row | cut -d':' -f1)
+        sample_data=$(echo $row | cut -d':' -f2-)
+        IFS=',' read -r -a row_array <<< "$sample_data"
+        # echo "debugging: ${row_array[*]}"
+        
+        # Add the ancestry values to the csv file based on the reference dataset
+        if [[ $REF == "Hapmap" ]]; then
+            row_array[13]=${top_ancestry}
+            row_array[14]=${ANCESTRY}
+        elif [[ $REF == "1KGenomes" ]]; then
+            row_array[15]=${top_ancestry}
+            row_array[16]=${ANCESTRY}
+        elif [[ $REF == "GrafPop" ]]; then
+            # to implement ...
+            echo "not implemented"
+        fi
 
-      # Update the csv file with the new ancestry values
-      new_row=$(IFS=,; echo "${row_array[*]}")
-      sed -i "${line_number}s|.*|$new_row|" "$CSV"
-      
-  done
+        # Update the csv file with the new ancestry values
+        for i in "${!row_array[@]}"; do
+          row_array[$i]=$(sanitize "${row_array[$i]}")
+        done
+
+        new_row=$(IFS=,; echo "${row_array[*]}")
+        sed -i "${line_number}s|.*|$new_row|" "$CSV"
+        
+    done
 fi
 
 if [ "$REF" = "GrafPop" ]; then
@@ -136,18 +142,6 @@ if [ "$REF" = "GrafPop" ]; then
     sample_data=$(echo $row | cut -d':' -f2-)
     IFS=',' read -r -a row_array <<< "$sample_data"
 
-    # row_array[4]=${GD1}
-    # row_array[5]=${GD2}
-    # row_array[6]=${GD3}
-    # row_array[7]=${GD4}
-    # row_array[8]=${P_f}
-    # row_array[9]=${P_e}
-    # row_array[10]=${P_a}
-    # row_array[11]=${PopID}
-    # row_array[12]=${Computed_Pop}
-    
-    # PopID=$(echo "$PopID" | tr -d '\r\n')
-    # PopID=$(sanitize "$PopID")
     
     row_array+=("$PopID")
 
